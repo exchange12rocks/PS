4902b4dee2334d08ab3596e41256a36262a31d62
Kirill Nikolaev <kf@exchange12rocks.org>
2017-04-23 03:13:09 +0300

    New tests. Improved appveyor.yml
    Some tests fails and this is normal at this development stage.

--- Test-DayProperties/Test-DayProperties.tests.ps1
+++ Test-DayProperties/Test-DayProperties.tests.ps1
@@ -1,6 +1,6 @@
 . (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath 'Test-DayProperties.ps1')
 
-Describe -Name 'Test Quarter' -Fixture {
+Describe -Name 'quarters' -Fixture {
 
     It -name "Months '<Months>' in a quarter '<Quarter>'" -TestCases @(
         @{Months = (1..3); Quarter = 1}
@@ -22,35 +22,47 @@ Describe -Name 'Test Quarter' -Fixture {
     }
 }
 
-Describe -Name 'Edge Cases' -Fixture {
+Describe -Name 'edge cases' -Fixture {
     Context -Name 'The First Day' -Fixture {
 
         $TheFirstDay = New-Object -TypeName DateTime -ArgumentList @(1, 1, 1)
 
-        It -name "The very first day is in the quarter '<Quarter>'" -TestCases @(
-            @{Quarter = 1}
+        It -name "The very first day is in the first quarter" -test {
+            Test-DayProperties -Quarter 1 -Date $TheFirstDay |
+            Should Be $true
+        }
+
+        It -name "The very first day is not in quarter #<Quarter>" -TestCases @(
+            @{Quarter = 2}
+            @{Quarter = 3}
+            @{Quarter = 4}
         ) -test {
             Param (
-                [ValidateSet(1)]
+                [ValidateRange(2,4)]
                 [int]$Quarter
             )
 
             Test-DayProperties -Quarter $Quarter -Date $TheFirstDay |
+            Should Be $false
+        }
+
+        It -name 'The very first day is the first Monday' -test { #http://www.academia.edu/1091577/What_day_of_the_week_was_01-01-0001 Gregorian calendar
+            Test-DayProperties -Date $TheFirstDay -DayOfWeek 1 -NumberInMonth 1 |
             Should Be $true
         }
 
-        It -name "The very first day is not in quarters '<Quarters>'" -TestCases @(
-            @{Quarters = (2..4)}
+        It -name "The very first day is not the Monday # <Num>" -TestCases @(
+            @{Num = 2},
+            @{Num = 3},
+            @{Num = 4},
+            @{Num = 5}
         ) -test {
-            Param (
-                [ValidateRange(2,4)]
-                [int[]]$Quarters
+            param (
+                [ValidateRange(2,5)]
+                [int]$Num
             )
 
-            foreach ($Quarter in $Quarters) {
-                Test-DayProperties -Quarter $Quarter -Date $TheFirstDay |
-                Should Be $false
-            }
+            Test-DayProperties -Date $TheFirstDay -DayOfWeek 1 -NumberInMonth $Num | Should Be $false
         }
     }
 
@@ -82,6 +94,46 @@ Describe -Name 'Edge Cases' -Fixture {
 
             Test-DayProperties -Quarter $Quarter -Date $TheLastDay |
             Should Be $true
+        }
+    }
+
+    Context -Name 'The First Week' -Fixture {
+        It -name "Day #<Num> on the very first week is the first day of this type in the month" -TestCases @(
+            @{Num = 1},
+            @{Num = 2},
+            @{Num = 3},
+            @{Num = 4},
+            @{Num = 5},
+            @{Num = 6},
+            @{Num = 7}
+        ) -test {
+            param (
+                [ValidateRange(1,7)]
+                [int]$Num
+            )
+
+            Test-DayProperties -Date (New-Object -TypeName DateTime -ArgumentList @(1, 1, $Num)) -DayOfWeek $Num -NumberInMonth 1 | Should Be $true
+        }
+    }
+
+    Context -Name 'The Last Week' -Fixture {
+        It -name "Day #<Num> on the very last week is the last day of this type in the month" -TestCases @(
+            @{Num = 1; Date = 27},
+            @{Num = 2; Date = 28},
+            @{Num = 3; Date = 29},
+            @{Num = 4; Date = 30},
+            @{Num = 5; Date = 31},
+            @{Num = 6; Date = 26},
+            @{Num = 7; Date = 25}
+        ) -test {
+            param (
+                [ValidateRange(1,7)]
+                [int]$Num,
+                [ValidateRange(25,31)]
+                [int]$Date
+            )
+
+            Test-DayProperties -Date (New-Object -TypeName DateTime -ArgumentList @(9999, 12, $Date)) -DayOfWeek $Num -Last | Should Be $true
         }
     }
 }
