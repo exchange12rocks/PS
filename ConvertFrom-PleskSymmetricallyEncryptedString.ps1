88f5e260474a26a3f9cfe61a7fea2ce129922131
Kirill Nikolaev <kf+GitHub@exchange12rocks.org>
2021-02-07 13:50:00 +0200

    ConvertFrom-PleskSymmetricallyEncryptedString: Adds comment-based help

--- ConvertFrom-PleskSymmetricallyEncryptedString.ps1
+++ ConvertFrom-PleskSymmetricallyEncryptedString.ps1
@@ -1,12 +1,39 @@
-# Thanks to:
-# https://mor-pah.net/2014/03/05/decrypt-plesk-11-passwords/
-# https://codeforcontent.com/blog/using-aes-in-powershell/
+<#
+.SYNOPSIS
+    Decrypts passwords symmetrically encrypted by Plesk on Windows.
+.DESCRIPTION
+    Plesk uses symmetrical encryption for many passwords in its internal MySQL database "psa". With the help of this script you can now decrypt them for Plesk running on Windows as well.
+.PARAMETER EncryptedString
+    An encrypted string you found Plesk's psa database
+.PARAMETER SymmetricKey
+    Plesk symmetric encryption key. You can find it at HKLM:\SOFTWARE\WOW6432Node\PLESK\PSA Config\Config\sym_key\sym_key, but this script extracts it automatically.
+.EXAMPLE
+    ConvertFrom-PleskSymmetricallyEncryptedString.ps1 -EncryptedString '$AES-128-CBC$ABNK35ZcqnbTYT4Q3mbaEA$HmGDWmtym6K3+kJ8uBoJOg'
+.OUTPUTS
+    [string]
+.NOTES
+    Author: Kirill Nikolaev
+    Twitter: @exchange12rocks
+    Web-site: https://exchange12rocks.org
+    GitHub: https://github.com/exchange12rocks
+.LINK
+    https://exchange12rocks.org/2021/02/08/how-to-decrypt-plesk-passwords-on-windows/
+.LINK
+    https://github.com/exchange12rocks/PS/blob/master/ConvertFrom-PleskSymmetricallyEncryptedString.ps1
+.LINK
+    https://mor-pah.net/2014/03/05/decrypt-plesk-11-passwords/
+.LINK
+    https://codeforcontent.com/blog/using-aes-in-powershell/
+#>
+
+#Requires -Version 3.0
 
 Param (
     [Parameter(Mandatory)]
     [string]$EncryptedString,
     [byte[]]$SymmetricKey = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\PLESK\PSA Config\Config\sym_key' -Name sym_key).sym_key
 )
+
 $EncryptedStringSplitted = $EncryptedString.Split('$')
 $IV = $EncryptedStringSplitted[2]
 $Data = $EncryptedStringSplitted[3]
